a05bc4031797522219e77984fc0a87f14b5faede
Abigail Trapp <abbsthepandahead@gmail.com>
2022-11-01 12:31:48 -0600

    JavaScript code commit

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function solution(inputArray) {
-
+    var x=0
+    var y=0
+    var max=-Infinity
+    for(var i=0;i<inputArray.length;i++){
+    x=inputArray[i]
+    y=inputArray[i+1]
+    if(x*y>max){
+        max=x*y
+    }
+    }
+    return max
 }
+console.log(solution([3,6,-2,-5,7,3]))
